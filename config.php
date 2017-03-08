114306551c3b023ce4e0ac414db142e213b9b3fb
Kirill Kholodilin <kirill.kholodilin@hitmeister.de>
2017-03-08 17:21:16 +0100

    fix conflicts

--- config.php
+++ config.php
@@ -77,8 +77,8 @@ return array(
 
         'aws'=> array(
             'bucket'=>'botuploads',
-            'key'=>'AKIAJ4QYZUUNWYYGRVTQ',
-            'secret'=>'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',
+            'key'=>'AKIAIUVDHFV34JWS33CA',
+            'secret'=>'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',
         ),
         'rabbit'=>array(
             'host'=>'localhost',
